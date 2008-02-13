5e2e6370bb4cf245168fa036e421e14e711246ea
mjhugo <mjhugo@832c1c66-4827-0410-8465-ccb17913cd09>
2008-02-13 03:55:25 +0000

    fixes for GRAILSPLUGINS-240

--- CodeCoverageGrailsPlugin.groovy
+++ CodeCoverageGrailsPlugin.groovy
@@ -1,37 +1,12 @@
 
 class CodeCoverageGrailsPlugin {
-    def version = '0.4.1'
-	def dependsOn = [core:'1.0-final-SNAPSHOT'] 
+    def version = '0.5'
+	def dependsOn = [core:'1.0'] 
 	def author = "Mike Hugo"
+	def authorEmail = "mike@piragua.com"
 	def title = "Generates Code Coverage reports"
 	def description = """
 		Creates Code Coverage reports for your code
 	"""
-	def documentation = "http://docs.codehaus.org/display/GRAILS/Test+Code+Coverage+Plugin"
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-   
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)		
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-	                                      
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-	
-    def onChange = { event ->
-        // TODO Implement code that is executed when this class plugin class is changed  
-        // the event contains: event.application and event.applicationContext objects
-    }
-                                                                                  
-    def onApplicationChange = { event ->
-        // TODO Implement code that is executed when any class in a GrailsApplication changes
-        // the event contain: event.source, event.application and event.applicationContext objects
-    }
+	def documentation = "http://grails.org/Test+Code+Coverage+Plugin"
 }
--- scripts/TestAppCobertura.groovy
+++ scripts/TestAppCobertura.groovy
@@ -1,3 +1,5 @@
+import net.sourceforge.cobertura.coveragedata.ProjectData
+
 Ant.property(environment:"env")
 grailsHome = Ant.antProject.properties."env.GRAILS_HOME"  
 
@@ -7,15 +9,34 @@ includeTargets << new File ( "${grailsHome}/scripts/TestApp.groovy" )
 
 pluginHome = codeCoveragePluginDir
 reportFormat = 'html'
+codeCoverageExclusionList = [
+    "**/*BootStrap*",
+    "**/*Config*",
+    "**/*DataSource*",
+    "**/*resources*",
+    "**/*UrlMappings*",
+    "**/*Tests*",
+    "**/PreInit*",
+    "CodeCoverageGrailsPlugin*" ]
 
-coverageReportDir = "${basedir}/test/reports/cobertura"
+// We need to save the exit code from the 'testApp' target.
+testAppExitCode = 0
 
 //TODO - would like this to work (put file in tmp dir), but cobertura creates a cobertura.ser in the running dir anyway...
 //dataFile = "${grailsTmp}/cobertura.ser"
 dataFile = "cobertura.ser"
 
 target ('default': "Test App with Cobertura") {
-    depends( classpath, checkVersion, configureProxy, packagePlugins )
+    depends(classpath, checkVersion, configureProxy)
+
+    // Check whether the project defines its own directory for test
+    // reports.
+    coverageReportDir = "${config.grails.testing.reports.destDir ?: testDir}/cobertura"
+
+    // Add any custom exclusions defined by the project.
+    if (config.coverage.exclusions) {
+      codeCoverageExclusionList += config.coverage.exclusions
+    }
 
     Ant.path(id: "cobertura.classpath"){
         fileset(dir:"${pluginHome}/lib"){
@@ -23,10 +44,10 @@ target ('default': "Test App with Cobertura") {
         }
     }
 
-	if(args?.indexOf('-xml') >-1) {
-		reportFormat = 'xml'
-		args -= '-xml'        
-	}
+    if(args?.indexOf('-xml') >-1) {
+        reportFormat = 'xml'
+        args -= '-xml'
+    }
 
     cleanup()
     packageApp()
@@ -36,17 +57,14 @@ target ('default': "Test App with Cobertura") {
 
     //per the Cobertura FAQ, force save of coverage data before JVM exits...
     //see http://cobertura.sourceforge.net/faq.html "Cobertura only writes the coverage data file when..."
-    try {
-        String className = "net.sourceforge.cobertura.coveragedata.ProjectData";
-        String methodName = "saveGlobalProjectData";
-        Class saveClass = Class.forName(className);
-        java.lang.reflect.Method saveMethod = saveClass.getDeclaredMethod(methodName, new Class[0]);
-        saveMethod.invoke(null,new Object[0]);
-    } catch (Throwable t) {}
+    ProjectData.saveGlobalProjectData()
 
     coberturaReport()
     Ant.delete(dir:testDirPath)
     event("StatusFinal", ["Cobertura Code Coverage Complete (view reports in: ${coverageReportDir})"])
+
+    // Exit the script using the code returned by 'testApp'.
+    System.exit(testAppExitCode)
 }
 
 target(cleanup:"Remove old files") {
@@ -63,13 +81,10 @@ target(instrumentTests:"Instruments the compiled test cases") {
         //and put that dir in front of testClasses in the classpath
         Ant.'cobertura-instrument' (datafile:"${dataFile}") {
             fileset(dir:testDirPath) {
-                exclude(name:"**/*BootStrap*")
-                exclude(name:"**/*Config*")
-                exclude(name:"**/*DataSource*")
-                exclude(name:"**/*resources*")
-                exclude(name:"**/*UrlMappings*")
-                exclude(name:"**/*Tests*")
                 include(name:"**/*.class")
+                codeCoverageExclusionList.each { pattern ->
+                    exclude(name:pattern)
+                }
             }
         }
     }
@@ -101,9 +116,7 @@ target(coberturaReport:"Generate Cobertura Reports") {
     }
 }
 
-target('exit':"override exit") { def code ->
-    //ignore '0' exit code to bypass TestApp.groovy exiting, process any other return code (e.g. -1)
-    if (0 != code){
-        System.exit(code)
-    }
+target('exit':"override exit") { code ->
+    // Save the exit code.
+    testAppExitCode = code
 }
