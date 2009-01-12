bb0193fede102956c1cc7f4517c2307126557611
mjhugo <mjhugo@832c1c66-4827-0410-8465-ccb17913cd09>
2009-01-12 19:25:00 +0000

    Rename events script to _Events

--- scripts/Events.groovy
+++
@@ -1,7 +0,0 @@
-eventSetClasspath = { rootLoader ->
-	if (System.properties.'cobertura.code.coverage') {
-		new File("${pluginHome}/lib/cobertura").eachFile {
-			rootLoader.addURL(it.toURL())
-		}
-	}
-}
