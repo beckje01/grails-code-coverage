1117b021b3f0ae5c826c589536a7e5ac03733d69
Jeff Beck <jeff.beck@reachlocal.com>
2013-02-21 08:14:10 -0600

    Updated to the latest release plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,33 +3,54 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// specify dependency exclusions here; for example, uncomment this to disable ehcache:
+		// excludes 'ehcache'
+	}
+	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
+	legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+		mavenCentral()
 
-        runtime 'net.sourceforge.cobertura:cobertura:1.9.4.1'
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-    }
+		//mavenRepo "http://repository.jboss.com/maven2/"
+	}
+	dependencies {
+
+
+		runtime 'net.sourceforge.cobertura:cobertura:1.9.4.1'
+	}
+
+	plugins {
+		runtime(":hibernate:$grailsVersion") {
+			export = false
+		}
+		build(":tomcat:$grailsVersion") {
+			export = false
+		}
+
+		build(":release:2.2.1") {
+			export = false
+		}
+	}
 }
+
+//<editor-fold desc="Release Plugin External Maven Config">
+def mavenConfigFile = new File("${basedir}/grails-app/conf/mavenInfo.groovy")
+if (mavenConfigFile.exists()) {
+	def slurpedMavenInfo = new ConfigSlurper().parse(mavenConfigFile.toURL())
+	slurpedMavenInfo.grails.project.repos.each {k, v ->
+		println "Adding maven info for repo $k"
+		grails.project.repos."$k" = v
+	}
+}
+else {
+	println "No mavenInfo file found."
+}
+//</editor-fold>
