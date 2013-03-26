1e980060b9659c9d62d9af281bac9a03f7553440
Jeff Beck <jeff.beck@reachlocal.com>
2013-03-26 10:21:06 -0500

    Update Plugin Descriptor this fixes #2

--- CodeCoverageGrailsPlugin.groovy
+++ CodeCoverageGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 
 class CodeCoverageGrailsPlugin {
-    def version = '1.2.6-SNAPSHOT'
+    def version = '1.2.6'
 
 	def grailsVersion = '1.2 > *'
 
@@ -11,10 +11,17 @@ class CodeCoverageGrailsPlugin {
 	def dependsOn = [core:'1.2 > *'] 
 	def author = "Mike Hugo"
 	def authorEmail = "mike@piragua.com"
+	def developers = [
+        [ name: "Jeff Beck" ]
+    ]
+
 	def title = "Generates Code Coverage reports"
 	def description = """
 		Creates Code Coverage reports for your code.
         Special thanks to Peter Ledbrook and Jeff Kunkle for contributions to this plugin.
 	"""
-	def documentation = "http://grails.org/Test+Code+Coverage+Plugin"
+	def documentation = "https://github.com/beckje01/grails-code-coverage"
+	def license = "APACHE"
+	def issueManagement = [system: "github", url: "https://github.com/beckje01/grails-code-coverage/issues"]
+	def scm = [url: "https://github.com/beckje01/grails-code-coverage"]
 }
