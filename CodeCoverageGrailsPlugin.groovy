7f155efef7ae18f3e8bfa1dd2cb0c09eac11b446
Burt Beckwith <burt@burtbeckwith.com>
2013-03-26 13:04:48 -0400

    cleanup

--- CodeCoverageGrailsPlugin.groovy
+++ CodeCoverageGrailsPlugin.groovy
@@ -1,25 +1,19 @@
-
 class CodeCoverageGrailsPlugin {
-    def version = '1.2.6'
+	def version = '1.2.6'
 
 	def grailsVersion = '1.2 > *'
 
 	def pluginExcludes = [
 		"grails-app/conf/mavenInfo.groovy"
 	]
-	
-	def dependsOn = [core:'1.2 > *'] 
-	def author = "Mike Hugo"
-	def authorEmail = "mike@piragua.com"
+
 	def developers = [
-        [ name: "Jeff Beck" ]
-    ]
+      [ name: "Mike Hugo", email: "mike@piragua.com" ],
+      [ name: "Jeff Beck" ]
+	]
 
 	def title = "Generates Code Coverage reports"
-	def description = """
-		Creates Code Coverage reports for your code.
-        Special thanks to Peter Ledbrook and Jeff Kunkle for contributions to this plugin.
-	"""
+	def description = "Creates Code Coverage reports for your code"
 	def documentation = "https://github.com/beckje01/grails-code-coverage"
 	def license = "APACHE"
 	def issueManagement = [system: "github", url: "https://github.com/beckje01/grails-code-coverage/issues"]
