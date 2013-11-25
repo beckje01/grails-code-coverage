2f7ebd2165c140d75c61661b6263d8121328c072
Jeff Beck <jeff.beck@reachlocal.com>
2013-11-25 11:06:37 -0600

    Remove external maven info slurping.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,17 +23,3 @@ grails.project.dependency.resolution = {
 		}
 	}
 }
-
-//<editor-fold desc="Release Plugin External Maven Config">
-def mavenConfigFile = new File(basedir, "grails-app/conf/mavenInfo.groovy")
-if (mavenConfigFile.exists()) {
-	def slurpedMavenInfo = new ConfigSlurper().parse(mavenConfigFile.toURI().toURL())
-	slurpedMavenInfo.grails.project.repos.each {k, v ->
-		println "Adding maven info for repo $k"
-		grails.project.repos."$k" = v
-	}
-}
-else {
-	println "No mavenInfo file found."
-}
-//</editor-fold>
